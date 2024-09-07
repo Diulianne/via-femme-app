72f57f1a92843dd2f12fc9dc00417e2d98182ad8
Diulianne Oliveira <diulicarine@gmail.com>
2024-09-07 18:41:23 -0300

    instalação de dependencias

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAbun3YlX8avk8S6J91Zx0npVDfWjHMhrM",
-    authDomain: "uber-next-clone-2da3f.firebaseapp.com",
-    projectId: "uber-next-clone-2da3f",
-    storageBucket: "uber-next-clone-2da3f.appspot.com",
-    messagingSenderId: "327502929961",
-    appId: "1:327502929961:web:fd6405a40f72bd1ca37c38"
+        apiKey: "AIzaSyAoSV5vfGtabOj9lMMJDwDQHFv1K-fTMeA",
+        authDomain: "via-femme-app.firebaseapp.com",
+        projectId: "via-femme-app",
+        storageBucket: "via-femme-app.appspot.com",
+        messagingSenderId: "145696557495",
+        appId: "1:145696557495:web:2314c406d1fe6df1e9c032"
+
 };
 
 // Initialize Firebase
